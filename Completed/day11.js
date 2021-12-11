c2ed03ca4ab1d79b0c7e67b0d375961ef70b06e6
Tyler <tylermacdonald88@gmail.com>
2021-12-11 10:40:10 -0500

    Removed an extra loop.

--- Completed/day11.js
+++ Completed/day11.js
@@ -11,12 +11,14 @@ let size = maxX*maxY;
 
 function flashRecurse(x,y){
     let d=[[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]; // All neighour Deltas
-    input[y][x] = "f";
+    input[y][x] = 0;
+    part1++;
+    part2++;
     for(let i=0; i<8; i++){
         let newX = x+d[i][0];
         let newY = y+d[i][1];
         if((newX>=0 && newX<maxX) && (newY>=0 && newY<maxY)){ //Make sure its in bounds
-            if(input[newY][newX]!="f"){ // Not already flashed
+            if(input[newY][newX]!=0){ // Not already flashed
                 input[newY][newX]++;
                 if(input[newY][newX]>=10){
                     flashRecurse(newX,newY); // flash if it should flash
@@ -27,11 +29,11 @@ function flashRecurse(x,y){
 }
 
 
-let flashCount = 0;
-let count=0;
+let part1 = 0;
+let part2=0;
 let step = 0;
-while(count<size){ //Exit loop when All flashes are found.
-    count=0;
+while(part2<size){ //Exit loop when All flashes are found.
+    part2=0;
     step++;
     input = input.map(y=>y.map(function(x){return x+1})); //increment entire grid
     for(let y=0; y<input.length; y++){
@@ -41,17 +43,8 @@ while(count<size){ //Exit loop when All flashes are found.
             }
         }
     }
-    for(let y=0; y<input.length; y++){
-        for(let x=0; x<input[y].length; x++){
-            if(input[y][x]=="f"){
-                input[y][x]=0;
-                flashCount++; // Part 1
-                count++; // Part 2
-            }
-        }
-    }
     if(step==100){ // Print Part 1 at step 100
-        console.log("Part 1: ", flashCount);
+        console.log("Part 1: ", part1);
     }
 }
 console.log("Part 2: ", step);
